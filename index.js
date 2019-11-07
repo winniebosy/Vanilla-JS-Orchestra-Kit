86e155bcb79c44bafde20d59f787b1995e9a028c
winniebosy <winniebosy@gmail.com>
2019-11-07 14:34:47 +0300

    query selector for selecting the key elements when pressed

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-//  lstening for a keyup event
+//  listening for a keyup event
+
 window.addEventListener('keydown', function(e){
-    this.console.log(e);
+    // selecting the data element
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+       console.log(audio);
+       if(!audio) return; //stops the function from running altogether
+       audio.play();
 
 });
